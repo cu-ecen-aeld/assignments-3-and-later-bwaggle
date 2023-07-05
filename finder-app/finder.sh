3ceacd94b4cd237c8c141dada2493019df9d51cb
bwaggle <bwaggle@Ubuntu.myguest.virtualbox.org>
2023-07-05 13:33:44 -0500

    Assignment 1

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+filesdir=$1
+searchstr=$2
+
+if test $# -ne 2
+then
+	echo "failed: must enter 2 arguments"
+	exit 1
+fi	
+
+
+if test ! -d "$filesdir" 
+then
+	echo "failed: expected a directory as the first argument"
+	exit 1
+fi
+
+
+file_count=$(find "$filesdir" -type f | wc -l)
+line_count=$(grep -r "$searchstr" $filesdir | wc -l)
+
+echo "The number of files are $file_count and the number of matching lines are $line_count"
---
+++ finder-app/writer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if test $# -ne 2
+then
+	echo "failed: must enter 2 arguments"
+	exit 1
+fi	
+
+writefile="$1"
+writestr="$2"
+
+mkdir -p "$(dirname "$writefile")"
+echo "$writestr" > "$writefile"
+
+if [ $? -ne 0 ]
+then
+  echo "Error: Failed to create the file"
+  exit 1
+fi
+
+exit 0
+
